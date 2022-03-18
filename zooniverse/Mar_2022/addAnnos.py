b10baaed487b21b8b10d70cebefce3af3fbb79d9
Glen Robson <glen.robson@gmail.com>
2022-03-18 15:29:15 +0000

    Testing latest version

---
+++ zooniverse/Mar_2022/addAnnos.py
@@ -0,0 +1,84 @@
+#! /usr/bin/env python3
+
+import json 
+from urllib import request
+import os
+import base64
+
+def loadJson(url):
+    with request.urlopen(url) as data:
+        return json.loads(data.read())
+
+# just a check to make sure that annoPages only link to one canvas 
+def checkOnlyOneCanvasInAnnoPage(annoPage):
+    canvases = []
+    for anno in annoPage['items']:
+        canvas = anno['target']['source']['id']
+        if canvas not in canvases:
+            canvases.append(canvas)
+
+    return len(canvases) < 2
+
+def addManifest(manifests, manifestURL):
+    manifest = loadJson(manifestURL)
+    manifests[manifestURL] = manifest
+
+def addAnnosToCanvas(manifest, annoPageURL):
+    canvasNo = 0
+    for canvas in manifest['sequences'][0]['canvases']:
+        print ('Adding annotations to canvas no: {}'.format(canvasNo))
+
+        # Add link to manifest pointing to annotation page
+        canvas['annotations'] = [{
+            "id": annoPageURL, # add the URL to the annotation page to the manifest
+            "type": "AnnotationPage"
+        }]
+
+        canvasNo += 1 
+
+def processAnnoPage(manifests, annoPageURL): 
+    print ('Proccessing {}'.format(annoPageURL))
+    annoPage = loadJson(annoPageURL)
+
+    if checkOnlyOneCanvasInAnnoPage(annoPage): 
+        if 'items' in annoPage and len(annoPage['items']) > 0:
+            target = annoPage['items'][0]['target']
+
+            canvas = target['source']['id']
+            manifestURL = target['source']['partOf']['id']
+
+            if manifestURL not in manifests:
+                addManifest(manifests, manifestURL)
+            
+            manifest = manifests[manifestURL]
+            # Add anno page to canvas
+            addAnnosToCanvas(manifest, annoPageURL)
+        else:
+            print ('Annotation page is empty')
+    else:
+        print ('Anno page {} contains more than one canvas'.format(annoPageURL))
+
+    if 'next' in annoPage:
+        processAnnoPage(manifests, annoPage['next']['id'])
+    
+
+if __name__ == "__main__":
+    base = "https://glenrobson.github.io/iiif_stuff/zooniverse/Mar_2022"
+
+    tAnnoCollection = loadJson('https://zooniverse.github.io/iiif-annotations/annotations/titles.json')
+
+    # Place to store downloaded Manifests that will be updated
+    manifests = {
+    }
+
+    processAnnoPage(manifests, tAnnoCollection['first']['id'])
+
+    # Write out the manifest with annotation links to file
+    for manifestURL in manifests:
+        splitManifestURL = manifestURL.split("/")
+        # This will only work with BL manifest URLs which have the id before tha manifest e.g:
+        # https://api.bl.uk/metadata/iiif/ark:/81055/vdc_100022589176.0x000002/manifest.json
+        filename = splitManifestURL[len(splitManifestURL) - 2] + ".json"
+        print ('Writing out {}'.format(filename))
+        with open(filename, 'w') as outfile:
+            json.dump(manifests[manifestURL], outfile)
---
+++ zooniverse/Mar_2022/index.md
@@ -0,0 +1,36 @@
+# Zooniverse Testing - partOf
+
+I created [this script](https://github.com/glenrobson/iiif_stuff/blob/master/zooniverse/partof/addAnnos.py) which will process all of the annotations from the [annotation collection](https://zooniverse.github.io/iiif-annotations/annotations/titles.json). It will:
+
+ * Download any manifests it finds
+ * and add a link to the AnnotationPage to each canvas.
+ * Follow the `next` link to the next AnnotationPage
+
+Looking at the edited [manifest](manifest.json) in Mirador:
+
+<div id="mirador" style="width: 100%; height: calc(100vh - 3px); position: relative;"></div>
+<script type='text/javascript' src='https://unpkg.com/mirador@latest/dist/mirador.min.js'></script>
+<script type="text/javascript">
+      var miradorInstance = Mirador.viewer({
+        id: 'mirador',
+        windows: [
+            {
+                manifestId: 'https://glenrobson.github.io/iiif_stuff/zooniverse/Mar_2022/manifest.json',
+                sideBarPanel: 'annotations',
+                sideBarOpen: true
+            }
+        ],
+      });
+</script>      
+
+<br/>
+
+Thanks to the fix from [dnoneill](https://github.com/dnoneill) it also works in [Annona](https://ncsu-libraries.github.io/annona/) which is really good for presenting your annotations. 
+
+<script src="https://ncsu-libraries.github.io/annona/dist/annona.js"></script>
+<link rel="stylesheet" type="text/css" href="https://ncsu-libraries.github.io/annona/dist/annona.css">
+
+<div id="storyboard">
+<iiif-storyboard annotationlist='https://zooniverse.github.io/iiif-annotations/annotations/titles/0.json'></iiif-storyboard>
+</div>
+
