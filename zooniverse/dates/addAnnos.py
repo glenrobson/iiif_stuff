e2e2f8d7e59ee09dd3976cece29ab8e04b63aec6
Glen Robson <glen.robson@gmail.com>
2022-04-01 12:46:56 +0100

    Adding date testing

---
+++ zooniverse/dates/addAnnos.py
@@ -0,0 +1,111 @@
+#! /usr/bin/env python3
+
+import json 
+from urllib import request
+from urllib.parse import urlparse
+from datetime import datetime
+import os
+import base64
+
+def loadJson(url):
+    with request.urlopen(url) as data:
+        return json.loads(data.read())
+
+# just a check to make sure that annoPages only link to one canvas 
+def checkOnlyOneCanvasInAnnoPage(annoPage):
+    canvases = []
+    for anno in annoPage['items']:
+        canvas = anno['target']['source']['id']
+        if canvas not in canvases:
+            canvases.append(canvas)
+
+    return len(canvases) < 2
+
+def addManifest(manifests, manifestURL):
+    manifest = loadJson(manifestURL)
+    manifests[manifestURL] = manifest
+
+def addAnnosToCanvas(manifest, canvasID, annoPageURL):
+    canvasNo = 0
+    for canvas in manifest['sequences'][0]['canvases']:
+        print ('Adding annotations to canvas no: {}'.format(canvasNo))
+
+        if canvas['@id'] == canvasID:
+            # Add link to manifest pointing to annotation page
+            canvas['annotations'] = [{
+                "id": annoPageURL, # add the URL to the annotation page to the manifest
+                "type": "AnnotationPage"
+            }]
+            break
+
+        canvasNo += 1 
+
+def processAnnoPage(manifests, annoPageURL, base): 
+    print ('Proccessing {}'.format(annoPageURL))
+    annoPage = loadJson(annoPageURL)
+    filename = os.path.basename(urlparse(annoPageURL).path)
+    newId = "{}/{}".format(base, filename)
+
+    if checkOnlyOneCanvasInAnnoPage(annoPage): 
+        if 'items' in annoPage and len(annoPage['items']) > 0:
+            target = annoPage['items'][0]['target']
+
+            canvas = target['source']['id']
+            manifestURL = target['source']['partOf']['id']
+
+            if manifestURL not in manifests:
+                addManifest(manifests, manifestURL)
+            
+            manifest = manifests[manifestURL]
+            # Add anno page to canvas
+            addAnnosToCanvas(manifest, canvas, newId)
+
+            # restructor annos
+            for anno in annoPage['items']:
+                oldBody = anno['body']
+                dateStr = oldBody['value']
+                try:
+                    date = datetime(int(dateStr[0:4]), int(dateStr[4:6]), int(dateStr[6:8]))
+                    
+                    newBody = {
+                        "type":"TextualBody",
+                        "value":"<time datetime=\"{}\">{}</time>".format(date.strftime("%Y-%m-%d"),date.strftime("%B, %d %Y")),
+                        "language":"en",
+                        "format" : "text/html"
+                    }
+
+                    anno['body'] = newBody
+                except ValueError:
+                    print ('Failed to parse {}'.format(dateStr))
+        else:
+            print ('Annotation page is empty')
+    else:
+        print ('Anno page {} contains more than one canvas'.format(annoPageURL))
+
+    with open(filename, 'w') as outfile:
+        json.dump(annoPage, outfile)
+    
+    if 'next' in annoPage:
+        processAnnoPage(manifests, annoPage['next']['id'], base)
+    
+
+if __name__ == "__main__":
+    base = "https://glenrobson.github.io/iiif_stuff/zooniverse/dates"
+
+    tAnnoCollection = loadJson('https://zooniverse.github.io/iiif-annotations/annotations/dates.json')
+
+    # Place to store downloaded Manifests that will be updated
+    manifests = {
+    }
+
+    processAnnoPage(manifests, tAnnoCollection['first']['id'], base)
+
+    # Write out the manifest with annotation links to file
+    for manifestURL in manifests:
+        splitManifestURL = manifestURL.split("/")
+        # This will only work with BL manifest URLs which have the id before tha manifest e.g:
+        # https://api.bl.uk/metadata/iiif/ark:/81055/vdc_100022589176.0x000002/manifest.json
+        filename = splitManifestURL[len(splitManifestURL) - 2] + ".json"
+        print ('Writing out {}'.format(filename))
+        with open(filename, 'w') as outfile:
+            json.dump(manifests[manifestURL], outfile)
---
+++ zooniverse/dates/index.md
@@ -0,0 +1,46 @@
+# Zooniverse Testing - dates
+
+I created [this script](https://github.com/glenrobson/iiif_stuff/blob/master/zooniverse/dates/addAnnos.py) which will process all of the annotations from the [dates annotation collection](https://zooniverse.github.io/iiif-annotations/annotations/dates.json). It will:
+
+ * Download any manifests it finds
+ * and add a link to the AnnotationPage to each canvas.
+ * Follow the `next` link to the next AnnotationPage
+ * Where it finds a valid date it will create the following annotation body:
+
+```
+"body": {
+    "format": "text/html",
+    "language": "en",
+    "type": "TextualBody",
+    "value": "<time datetime=\"1819-02-02\">February, 02 1819</time>"
+},
+```
+
+The [manifest](vdc_100022589176.0x000002.json) and [example annotation page](0.json):
+
+<div id="mirador" style="width: 100%; height: calc(100vh - 3px); position: relative;"></div>
+<script type='text/javascript' src='https://unpkg.com/mirador@latest/dist/mirador.min.js'></script>
+<script type="text/javascript">
+      var miradorInstance = Mirador.viewer({
+        id: 'mirador',
+        windows: [
+            {
+                manifestId: 'https://glenrobson.github.io/iiif_stuff/zooniverse/dates/vdc_100022589176.0x000002.json',
+                sideBarPanel: 'annotations',
+                sideBarOpen: true
+            }
+        ],
+      });
+</script>      
+
+<br/>
+
+Thanks to the fix from [dnoneill](https://github.com/NCSU-Libraries/annona/issues/35) it also works in [Annona](https://ncsu-libraries.github.io/annona/) which is really good for presenting your annotations. 
+
+<script src="https://ncsu-libraries.github.io/annona/dist/annona.js"></script>
+<link rel="stylesheet" type="text/css" href="https://ncsu-libraries.github.io/annona/dist/annona.css">
+
+<div id="storyboard">
+<iiif-storyboard annotationlist='https://github.com/glenrobson/iiif_stuff/blob/master/zooniverse/dates/0.json'></iiif-storyboard>
+</div>
+
